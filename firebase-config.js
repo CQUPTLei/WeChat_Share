28e791a4390317c99f699ec021a9d10e8267c693
dahu <1413454867@qq.com>
2025-05-31 03:24:10 +0800

    change firestore config

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyAOBvOeOWkRWWssJ0V0c-ge4j_YgNT2_Z4", // Replace with your actual API key if different
+    authDomain: "resource-sharing-center.firebaseapp.com",
+    projectId: "resource-sharing-center",
+    storageBucket: "resource-sharing-center.firebasestorage.app",
+    messagingSenderId: "846208459960",
+    appId: "1:846208459960:web:b874dc71135c1217be004d",
+    measurementId: "G-W606DJX5LW" // Optional: if you use Google Analytics
+};
